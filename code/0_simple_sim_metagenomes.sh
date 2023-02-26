1e25d7091a6d66be9a0df1ad0e2bd2803dcb5089
EisenRa <raph.eisenhofer@gmail.com>
2023-02-26 10:40:17 +1030

    .

--- code/0_simple_sim_metagenomes.sh
+++ code/0_simple_sim_metagenomes.sh
@@ -2,8 +2,10 @@
 # Raphael Eisenhofer Feb/2023
 
 # Fetch Zymo mock reference genomes:
+mkdir data/simple_sim_metagenomes/genomes && cd data/simple_sim_metagenomes/genomes
 wget https://zenodo.org/record/3935737/files/ZymoBIOMICS.STD.refseq.v2.zip?download=1
 mv ZymoBIOMICS.STD.refseq.v2.zip\?download\=1 ZymoBIOMICS.STD.refseq.v2.zip && unzip ZymoBIOMICS.STD.refseq.v2.zip
+rm ZymoBIOMICS.STD.refseq.v2.zip
 
 # Create simulated metagenomes using https://github.com/merenlab/reads-for-assembly.git
 git clone merenlab/reads-for-assembly.git
@@ -11,3 +13,11 @@ git clone merenlab/reads-for-assembly.git
 # Create ANVIO conda environment and load it (https://anvio.org/install/)
 
 # Create the simulated metagenomes:
+./reads-for-assembly/gen-paired-end-reads ../../../code/simulated_metagenome_profiles/sim_meta_1a.ini
+./reads-for-assembly/gen-paired-end-reads ../../../code/simulated_metagenome_profiles/sim_meta_1b.ini
+./reads-for-assembly/gen-paired-end-reads ../../../code/simulated_metagenome_profiles/sim_meta_2a.ini
+./reads-for-assembly/gen-paired-end-reads ../../../code/simulated_metagenome_profiles/sim_meta_3a.ini
+
+gzip simple_simulated_metagenomes/*.fastq
+cp simple_simulated_metagenomes/sim_meta_1a-R1.fastq.gz simple_simulated_metagenomes/sim_meta_1aa-R1.fastq.gz 
+cp simple_simulated_metagenomes/sim_meta_1a-R2.fastq.gz simple_simulated_metagenomes/sim_meta_1aa-R2.fastq.gz 
